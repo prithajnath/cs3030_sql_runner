8112d8dc9a539ea80ddb0e6d58d8e68c45fdc6b1
Prithaj Nath <prithajnath@gmail.com>
2025-11-15 17:36:23 -0500

    rm not remove

--- run_oracledb.sh
+++ run_oracledb.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Checking for existing oracle db image"
 [ -z $(docker image ls | grep oracle) ] && echo "No image found. Pulling from registry" && docker pull container-registry.oracle.com/database/free:latest
 echo "Checking for existing oracle db container"
-if [ -n "$(docker ps -a | grep oracle-db)" ]; then echo "Found a running container. Killing and restarting..." && docker stop oracle-db && docker remove oracle-db; fi
+if [ -n "$(docker ps -a | grep oracle-db)" ]; then echo "Found a running container. Killing and restarting..." && docker stop oracle-db && docker rm oracle-db; fi
 docker run -p 1521:1521 -d --name oracle-db container-registry.oracle.com/database/free:latest
 while ! docker ps --filter "name=oracle-db" --format '{{.Status}}' | grep -q 'healthy'; do
     echo "Waiting for db container to start accepting connections..."
